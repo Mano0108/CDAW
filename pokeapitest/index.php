b6d2174ad753446750f012df4482eed5d8bd075a
HarivelGabriel <harivelgabriel@gmail.com>
2022-11-18 08:56:40 +0100

    code more readable

--- pokeapitest/index.php
+++ pokeapitest/index.php
@@ -1,28 +1,32 @@
 <html>
-    <h1> Exercice PokeAPI</h1>
-    <br>
-    <br>
-    <table>
+<h1> Exercice PokeAPI</h1>
+<br>
+<br>
+<table>
     <thead>
         <tr>
-            <th>pokemon</th>
+            <td>id</td>
+            <td>Name</td>
+            <td>Image</td>
         </tr>
     </thead>
     <tbody>
         <?php
-            for($i=1; $i<10; $i++){
-                $file = file_get_contents("https://pokeapi.co/api/v2/pokemon/$i");
-                $json = json_decode($file, true);
-                
-                $name = $json['name'];
-                $image = $json['sprites']['front_default'];
+        $file = file_get_contents("https://pokeapi.co/api/v2/pokemon/?limit=208&offset=0");
+        $pokemons = json_decode($file, true);
+        for ($i=0; $i < 20; $i++) {
+            $name = $pokemons[$i]['name'];
+            $pkmn = file_get_contents("https://pokeapi.co/api/v2/pokemon/$i");
+            $image = $pkmn['sprites']['front_default'];
 
-                echo "<tr><td> $i </td><td> $name </td><td> <img src='$image' alt='alternatetext'> </td> </tr>";
-            }
-            ?>
-        <tr>
-        </tr>
+            echo "<tr>";
+            echo "<td> $i </td>";
+            echo "<td> $name </td>";
+            echo "<td> <img src='$image' alt='alternatetext'> </td>";
+            echo "</tr>";
+        }
+        ?>
     </tbody>
-</table> 
-</html>
+</table>
 
+</html>
