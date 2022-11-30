3ec3e86055f776a81a2266f5c4a49b6cd2f18b43
HarivelGabriel <harivelgabriel@gmail.com>
2022-11-30 12:50:42 +0100

    add gradient cover

--- pokemon/resources/views/mainMenu.blade.php
+++ pokemon/resources/views/mainMenu.blade.php
@@ -1,5 +1,13 @@
 @extends('template')
 
+@section('style')
+<link rel="stylesheet" href="{{ URL::asset('css/gradient.css') }} " type="text/css">
+@endsection
 @section('content')
 
 <p>Welcome {{$user->name}}</p>
+
+@endsection
+
+@section('script')
+@endsection
