532397468c9fa0c65919f18e62740a5c0ab640c7
HarivelGabriel <harivelgabriel@gmail.com>
2022-12-09 17:40:25 +0100

    Changed navbar btn order for better UX

--- pokemon/resources/views/tools/navbar.blade.php
+++ pokemon/resources/views/tools/navbar.blade.php
@@ -4,12 +4,8 @@
 
 <div class="navbar">
     <div class="navbtn navbtn-border">
-        <img class="navbar-icon" src="{{ URL::asset('/images/navbar/pokedex.png') }}">
-        <a href="/menu/pokedex">POKEDEX</a>
-    </div>
-    <div class="navbtn navbtn-border">
-        <img class="navbar-icon" src="{{ URL::asset('/images/navbar/replays.png') }}">
-        <a href="/menu/replays">REPLAYS</a>
+        <img class="navbar-icon" src="{{ URL::asset('/images/navbar/info-64.png') }}">
+        <a href="/menu/info">INFO</a>
     </div>
     <div class="navbtn navbtn-border">
         <img class="navbar-icon" src="{{ URL::asset('images/navbar/fight.png') }}">
@@ -19,9 +15,13 @@
         <img class="navbar-icon" src="{{ URL::asset('images/navbar/team.png') }}">
         <a href="/menu/team">TEAM</a>
     </div>
+    <div class="navbtn navbtn-border">
+        <img class="navbar-icon" src="{{ URL::asset('/images/navbar/pokedex.png') }}">
+        <a href="/menu/pokedex">POKEDEX</a>
+    </div>
     <div class="navbtn">
-        <img class="navbar-icon" src="{{ URL::asset('/images/navbar/info-64.png') }}">
-        <a href="/menu/info">INFO</a>
+        <img class="navbar-icon" src="{{ URL::asset('/images/navbar/replays.png') }}">
+        <a href="/menu/replays">REPLAYS</a>
     </div>
 </div>
 
