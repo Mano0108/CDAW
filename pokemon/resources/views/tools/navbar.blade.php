70fdc713c4fc593b66a019e053d8379dce485966
HarivelGabriel <harivelgabriel@gmail.com>
2022-12-12 16:13:51 +0100

    fixed navbar button target issue

--- pokemon/resources/views/tools/navbar.blade.php
+++ pokemon/resources/views/tools/navbar.blade.php
@@ -2,27 +2,33 @@
     <link rel="stylesheet" href="{{ URL::asset('css/tools/navbar.css') }} " type="text/css">
 @endpush
 
+<form action="/menu/info" method="GET" id="form-info" display="hidden"></form>
+<form action="/menu/fight" method="GET" id="form-fight" display="hidden"></form>
+<form action="/menu/team" method="GET" id="form-team" display="hidden"></form>
+<form action="/menu/pokedex" method="GET" id="form-pokedex" display="hidden"></form>
+<form action="/menu/replays" method="GET" id="form-replays" display="hidden"></form>
+
 <div class="navbar">
-    <div class="navbtn navbtn-border {{$menu['info']}}">
+    <button class="navbtn navbtn-border {{$menu['info']}}" type="submit" form="form-info">
         <img class="navbar-icon" src="{{ URL::asset('/images/navbar/info-64.png') }}">
-        <a href="/menu/info">INFO</a>
-    </div>
-    <div class="navbtn navbtn-border {{$menu['fight']}}">
+        <p>INFO</p>
+    </button>
+    <button class="navbtn navbtn-border {{$menu['fight']}}"  type="submit" form="form-fight">
         <img class="navbar-icon" src="{{ URL::asset('images/navbar/fight.png') }}">
-        <a href="/menu/fight">FIGHT</a>
-    </div>
-    <div class="navbtn navbtn-border {{$menu['team']}}">
+        <p>FIGHT</p>
+    </button>
+    <button class="navbtn navbtn-border {{$menu['team']}}" type="submit" form="form-team">
         <img class="navbar-icon" src="{{ URL::asset('images/navbar/team.png') }}">
-        <a href="/menu/team">TEAM</a>
-    </div>
-    <div class="navbtn navbtn-border {{$menu['pokedex']}}">
+        <p>TEAM</p>
+    </button>
+    <button class="navbtn navbtn-border {{$menu['pokedex']}}" type="submit" form="form-pokedex">
         <img class="navbar-icon" src="{{ URL::asset('/images/navbar/pokedex.png') }}">
-        <a href="/menu/pokedex">POKEDEX</a>
-    </div>
-    <div class="navbtn {{$menu['replays']}}">
+        <p>POKEDEX</p>
+    </button>
+    <button class="navbtn {{$menu['replays']}}" type="submit" form="form-replays">
         <img class="navbar-icon" src="{{ URL::asset('/images/navbar/replays.png') }}">
-        <a href="/menu/replays">REPLAYS</a>
-    </div>
+        <p>REPLAYS</p>
+    </button>
 </div>
 
 @push('script')
