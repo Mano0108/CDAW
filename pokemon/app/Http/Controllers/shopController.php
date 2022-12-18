af1dc2990e6b33697a77d9ae4294115998a56afc
Manoj Alagesan <alagesan.manoj@gmail.com>
2022-12-18 23:00:02 +0100

    disable button if not enogu gold. End of the Feature SHOP

--- pokemon/app/Http/Controllers/shopController.php
+++ pokemon/app/Http/Controllers/shopController.php
@@ -9,20 +9,25 @@ use Illuminate\Support\Facades\URL;
 
 class shopController extends Controller
 {
-    public function __invoke(Request $energy_id)
+    //param : energy_id we wish to unlock
+    public function __invoke(Request $request)
     {
+        
         $user = auth()->user();
-        if ($user->gold>80 ){
+        //if enough gold, add the energy to the user and update users gold 
+        if ($user->gold > 80 ){
             DB::table('user_energy')->insert([
-                'FK_energy' => $energy_id , 
+                'FK_energy' => $request->energy_id , 
                 'FK_user' => $user->id
             ]); 
             DB::table('users')
                 ->where('id',$user->id)
                 ->update([
                     'gold'=>$user->gold-80
-            ]);        
+            ]);  
+      
         }
         return redirect('/menu/shop');
+
     }
 }
