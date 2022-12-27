ad6226a57e739206b61acfbd79cf4b5f12452075
Manoj Alagesan <alagesan.manoj@gmail.com>
2022-12-27 19:02:07 +0100

    css for navbar (gold count)

--- pokemon/resources/views/tools/drop-button.blade.php
+++ pokemon/resources/views/tools/drop-button.blade.php
@@ -1,21 +1,22 @@
 @push('style')
     <link rel="stylesheet" href="{{ URL::asset('css/tools/drop-button.css') }} " type="text/css">
 @endpush
-
-<div class="dropdown">
+<div class="dropdown-container">
+    <div class="dropdown">
             
-    <button class="dropbtn">
-        <img id ="connected" src="{{URL::asset('/images/navbar/status.png')}}" >
-        <p>{{ $user->name }}</p>
-    </button>
-    <div class="dropdown-content">
-        <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-            {{ __('Profile') }}
-        </x-jet-responsive-nav-link>
-        <!-- unable to send post request with x-jet-responsive-nav-link so i tricoted this logout form-->
-        <form method="POST" action="{{ route('logout') }}" x-data>
-            @csrf
-            <input id="logout" type="submit" value="Log out">
-        </form>
+        <button class="dropbtn">
+            <img id ="connected" src="{{URL::asset('/images/navbar/status.png')}}" >
+            <p>{{ $user->name }}</p>
+        </button>
+        <div class="dropdown-content">
+            <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                {{ __('Profile') }}
+            </x-jet-responsive-nav-link>
+            <!-- unable to send post request with x-jet-responsive-nav-link so i tricoted this logout form-->
+            <form method="POST" action="{{ route('logout') }}" x-data>
+                @csrf
+                <input id="logout" type="submit" value="Log out">
+            </form>
+        </div>
     </div>
 </div>
